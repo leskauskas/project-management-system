b20dd39913ae7365a4011ba892fb64d16d503345
cipherswan <leskauskas25@gmail.com>
2018-11-26 20:55:55 +0200

    Navbar user access fix

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,8 +1,10 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
+        @guest
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'PMSys') }}
         </a>
+        @endguest
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
