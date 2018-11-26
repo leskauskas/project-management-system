f364cd87ef3716e2416ffa4cead68a85e174118a
cipherswan <leskauskas25@gmail.com>
2018-11-26 21:20:37 +0200

    Project tasks preperation

--- app/Project.php
+++ app/Project.php
@@ -9,4 +9,8 @@ class Project extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function tasks(){
+        return $this->hasMany('App\Task');
+    }
 }
