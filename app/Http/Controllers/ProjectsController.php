722e8b5dd62e33bdf68264c7b3c49a4d32caf80f
cipherswan <leskauskas25@gmail.com>
2018-11-26 21:11:30 +0200

    Access control

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,6 +9,16 @@ use App\Project;
 class ProjectsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
