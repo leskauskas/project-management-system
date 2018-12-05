896ddcc23cc42df731b7388463bf69a9af73a04d
cipherswan <leskauskas25@gmail.com>
2018-12-05 19:53:48 +0200

    Basic user navigation

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,3 +1,5 @@
+@guest
+
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
         @guest
@@ -53,4 +55,87 @@
             </ul>
         </div>
     </div>
-</nav>
+</nav> 
+
+@else
+<header>
+    <nav id="side-nav">
+        <a href="#"><i class="fas fa-home"></i><span>Dashboard</span></a>
+        <a href="#"><i class="fas fa-columns"></i><span>Kanban</span></a>
+        <a href="#"><i class="fas fa-chart-line"></i><span>Analytics</span></a>
+    </nav>
+    <nav id="top-nav">
+            {{ Auth::user()->name }} <span class="caret"></span>
+            <a href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+    </nav>
+</header>
+@endguest
+
+
+
+
+
+{{-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        @guest
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'PMSys') }}
+        </a>
+        @endguest
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        @if (Route::has('register'))
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        @endif
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>  --}}
+
