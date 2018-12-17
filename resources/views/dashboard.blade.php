220b101425c657fcf944111c8b86b31d4b9b760d
cipherswan <leskauskas25@gmail.com>
2018-12-17 15:06:35 +0200

    Dashboard design progress

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,27 +36,34 @@
     </div>
 
     <div class="row">
-        <div class="col-8">
+
+        <div class="col-8 dashboardProjectsColumn">
             <h5>Projects
                 <button type="button" class="btn btn-primary ml-1" data-toggle="modal" data-target="#newProjModal">+</button>
             </h5>
             @if (count($projects) > 0)
-                <ul class="list-group mt-3">
-                    @foreach ($projects as $proj)
-                        <li class="list-group-item">
-                            <h5><a href="/projects/{{$proj->id}}">{{$proj->name}}</a></h5>
-                            <h6><b>Due: </b>{{$proj->due_date}}</h6>
+                
+                @foreach ($projects as $proj)
+                    <a href="/projects/{{$proj->id}}">
+                        <div class="globalCard projectCard mb-2">
+                            <div class="projectCard-title">
+                                <h5>{{$proj->name}}</h5>
+                            <span class="globalBadge">{{$proj->priority}}</span>
+                            </div> 
+                            <p><b>Due date: </b>{{$proj->due_date}}</p>
                             <div class="progress" style="height: 5px">
                                 <div class="progress-bar" role="progressbar" aria-valuenow="{{$proj->getProjectProgress()}}" aria-valuemin="0" aria-valuemax="100" style="width: {{$proj->getProjectProgress()}}%; height: 5px"></div>
                             </div>
-                        </li>
-                    @endforeach
-                </ul>
+                        </div>   
+                    </a>
+                @endforeach
+                
             @else
                 <p>No projects yet</p>
             @endif 
         </div>
-        <div class="col-4">
+
+        <div class="col-4 dashboardCheckColumn">
             <h5>My personal checklist</h5>
             {!! Form::open(['action' => 'ChecklistsController@store', 'method' => 'POST']) !!}
                 <div class="form-group">
@@ -91,6 +98,8 @@
                 @endforeach
                 
         </div>
+
     </div>
+
 </div>
 @endsection
