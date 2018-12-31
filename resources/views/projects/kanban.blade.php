bc97b6dbfd54929a24c8b7051ca7ace868289ee7
cipherswan <leskauskas25@gmail.com>
2018-12-31 11:11:47 +0200

    Kanban board layout improvement

--- resources/views/projects/kanban.blade.php
+++ resources/views/projects/kanban.blade.php
@@ -10,13 +10,14 @@
             </div>
             @foreach ($project->tasks as $t)
                 @if ($t->status == 'not started')
-                    <div class="globalCard p-3 mb-2">
+                    <div class="globalCard p-2 mb-2">
                         <p class="m-0">{{$t->task_name}}</p>
-                        <hr class="my-1">
+                        <span class="badge badge-danger task-due"><i class="far fa-clock"></i> {{$t->due_date}}</span>
                     </div>
                 @endif
             @endforeach
         </div>
+
         <div class="column inProgress">
             <div class="columnHeader">
                 <h5 class="text-muted">In Progress</h5>
@@ -24,13 +25,14 @@
             </div>
             @foreach ($project->tasks as $t)
                 @if ($t->status == 'in progress')
-                    <div class="globalCard mb-2">
+                    <div class="globalCard p-2 mb-2">
                         <p class="m-0">{{$t->task_name}}</p>
-                        <hr class="my-1"> 
+                        <span class="badge badge-danger task-due"><i class="far fa-clock"></i> {{$t->due_date}}</span>
                     </div>   
                 @endif
             @endforeach
         </div>
+
         <div class="column done">
             <div class="columnHeader">
                 <h5 class="text-muted">Done</h5>
@@ -38,9 +40,9 @@
             </div>
             @foreach ($project->tasks as $t)
                 @if ($t->status == 'done')
-                    <div class="globalCard mb-2">    
+                    <div class="globalCard p-2 mb-2">    
                         <p class="m-0">{{$t->task_name}}</p>
-                        <hr class="my-1">
+                        <span class="badge badge-danger task-due"><i class="far fa-clock"></i> {{$t->due_date}}</span>
                     </div>  
                 @endif
             @endforeach
