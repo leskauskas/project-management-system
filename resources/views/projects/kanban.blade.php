ab4035e680f872d0afae101bcf2224ff7a4d5939
cipherswan <leskauskas25@gmail.com>
2018-12-31 10:16:59 +0200

    kanban board design progress

--- resources/views/projects/kanban.blade.php
+++ resources/views/projects/kanban.blade.php
@@ -5,36 +5,42 @@
 
         <div class="column notStarted">
             <div class="columnHeader">
-                <h4>Not Started</h4>
+                <h5 class="text-muted">Not Started</h5>
+                <hr class="my-2">
             </div>
             @foreach ($project->tasks as $t)
                 @if ($t->status == 'not started')
-                    <div class="globalCard mb-2">
-                        <p>{{$t->task_name}}</p>  
+                    <div class="globalCard p-3 mb-2">
+                        <p class="m-0">{{$t->task_name}}</p>
+                        <hr class="my-1">
                     </div>
                 @endif
             @endforeach
         </div>
         <div class="column inProgress">
             <div class="columnHeader">
-                <h4>In Progress</h4>
+                <h5 class="text-muted">In Progress</h5>
+                <hr class="my-2">
             </div>
             @foreach ($project->tasks as $t)
                 @if ($t->status == 'in progress')
                     <div class="globalCard mb-2">
-                        <p>{{$t->task_name}}</p>  
+                        <p class="m-0">{{$t->task_name}}</p>
+                        <hr class="my-1"> 
                     </div>   
                 @endif
             @endforeach
         </div>
         <div class="column done">
             <div class="columnHeader">
-                <h4>Done</h4>
+                <h5 class="text-muted">Done</h5>
+                <hr class="my-2">
             </div>
             @foreach ($project->tasks as $t)
                 @if ($t->status == 'done')
-                    <div class="globalCard mb-2">
-                        <p>{{$t->task_name}}</p>  
+                    <div class="globalCard mb-2">    
+                        <p class="m-0">{{$t->task_name}}</p>
+                        <hr class="my-1">
                     </div>  
                 @endif
             @endforeach
