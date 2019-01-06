de727a79097a11475ea6af44a419b1494e81258c
cipherswan <leskauskas25@gmail.com>
2019-01-06 23:25:38 +0200

    Dynamic due date on Kanban board

--- resources/views/projects/kanban.blade.php
+++ resources/views/projects/kanban.blade.php
@@ -13,7 +13,30 @@
                 @if ($t->status == 'not started')
                     <div class="globalCard p-2 mb-2">
                         <p class="m-0">{{$t->task_name}}</p>
-                        <span class="badge badge-danger task-due"><i class="far fa-clock"></i> {{$t->due_date}}</span>
+                        @php
+                            $today = date("Y-m-d");
+                            $dueDate = date("Y-m-d", strtotime($t->due_date));
+                            $due = false;
+
+                            $date1=date_create($today);
+                            $date2=date_create($dueDate);
+                            $diff=date_diff($date1,$date2);
+                    
+                            if ((int)$diff->format("%r%a") >= 0 && (int)$diff->format("%r%a") <=5 ) {
+                                $due = true;
+                            }
+                        @endphp
+                        <task-due duedate="{{date('Y-m-d', strtotime($t->due_date))}}" isdue={{$due}}></task-due>
+                        <span class="badge">
+                            @php
+                                if ((int)$diff->format("%r%a") < 0) {
+                                    echo 'Past deadline';
+                                }
+                                else{
+                                    echo $diff->format("%d days");
+                                }
+                            @endphp         
+                        </span>
                     </div>
                 @endif
             @endforeach
@@ -28,8 +51,31 @@
                 @if ($t->status == 'in progress')
                     <div class="globalCard p-2 mb-2">
                         <p class="m-0">{{$t->task_name}}</p>
-                        <span class="badge badge-danger task-due"><i class="far fa-clock"></i> {{$t->due_date}}</span>
-                    </div>   
+                        @php
+                            $today = date("Y-m-d");
+                            $dueDate = date("Y-m-d", strtotime($t->due_date));
+                            $due = false;
+
+                            $date1=date_create($today);
+                            $date2=date_create($dueDate);
+                            $diff=date_diff($date1,$date2);
+                    
+                            if ((int)$diff->format("%r%a") >= 0 && (int)$diff->format("%r%a") <=5 ) {
+                                $due = true;
+                            }
+                        @endphp
+                        <task-due duedate="{{date('Y-m-d', strtotime($t->due_date))}}" isdue={{$due}}></task-due>
+                        <span class="badge">
+                            @php
+                                if ((int)$diff->format("%r%a") < 0) {
+                                    echo 'Past deadline';
+                                }
+                                else{
+                                    echo $diff->format("%d days");
+                                }
+                            @endphp         
+                        </span>
+                    </div>
                 @endif
             @endforeach
         </div>
@@ -43,7 +89,30 @@
                 @if ($t->status == 'done')
                     <div class="globalCard p-2 mb-2">    
                         <p class="m-0">{{$t->task_name}}</p>
-                        <span class="badge badge-danger task-due"><i class="far fa-clock"></i> {{$t->due_date}}</span>
+                        @php
+                            $today = date("Y-m-d");
+                            $dueDate = date("Y-m-d", strtotime($t->due_date));
+                            $due = false;
+
+                            $date1=date_create($today);
+                            $date2=date_create($dueDate);
+                            $diff=date_diff($date1,$date2);
+                    
+                            if ((int)$diff->format("%r%a") >= 0 && (int)$diff->format("%r%a") <=5 ) {
+                                $due = true;
+                            }
+                        @endphp
+                        <task-due duedate="{{date('Y-m-d', strtotime($t->due_date))}}" isdue={{$due}}></task-due>
+                        <span class="badge">
+                            @php
+                                if ((int)$diff->format("%r%a") < 0) {
+                                    echo 'Past deadline';
+                                }
+                                else{
+                                    echo $diff->format("%d days");
+                                }
+                            @endphp         
+                        </span>
                     </div>  
                 @endif
             @endforeach
